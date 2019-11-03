931917aa936f3cdfa380d264b8229cc66af2aaf9
dwaffe <dwaffe@gmail.com>
2019-11-03 11:07:31 +0100

    servo test

--- Test.py
+++ Test.py
@@ -1,8 +1,15 @@
 from picamera import PiCamera
+from gpiozero import Servo
 from time import sleep
+
+servoOne = Servo(22)
+servoTwo = Servo(27)
+
+servoOne.mid()
+servoTwo.mid()
 
 camera = PiCamera()
 camera.start_preview()
-sleep(5)
+sleep(0.5)
 camera.capture('picture.jpg')
 camera.stop_preview()
